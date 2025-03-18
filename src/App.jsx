7870d93192049a919113f43f8b343650556c3d4e
Jonelle <jonelcubio021@gmail.com>
2025-03-18 22:04:04 +0800

    Initial commit: Tenzies

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Main } from "./components/Main";
+
+export function App() {
+  return(
+   <>
+   <Main />
+   </>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,5 @@
+export function Main() {
+  return(
+    <main className="main-container">Meow</main>
+  );
+}
