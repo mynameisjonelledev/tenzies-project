916335cae09a4a5345597edcf16e34b940510e77
Jonelle <jonelcubio021@gmail.com>
2025-03-19 21:57:06 +0800

    Tenzies : Generate 10 random numbers

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,22 @@
 import { Die } from "./Die";
-import dice1 from "../assets/one.svg"
-import dice2 from "../assets/two.svg"
-import dice3 from "../assets/three.svg"
-import dice4 from "../assets/four.svg"
-import dice5 from "../assets/five.svg"
-import dice6 from "../assets/six.svg"
-
 import { useState } from "react";
 
 export function Main() {
 
-  /* const diceImages = [dice1, dice2, dice3, dice4, dice5, dice6];
+  function generateAllNewDice() {
+    const generatedArray = [];
 
-  const [diceNumber, setDiceNumber] = useState(1);
+    for(let i = 0; i < 10; i++){
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      generatedArray.push(randomNumber);
+    }
+
+    return generatedArray;
+  }
+
+  console.log(generateAllNewDice());
 
   
-  const rollDice = () => {
-    const randomNumber = Math.floor(Math.random() * 6) + 1;
-    setDiceNumber(randomNumber);
-  } */
-
   return(
     <main className="main-container">
       <div className="dice-container">
